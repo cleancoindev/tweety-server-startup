443e124064686bd1123d4a3f25aa42e8eba7267b
Ian Atha <ian@autma.com>
2013-11-12 17:24:13 -0800

    automatically install service

--- tweety.go
+++ tweety.go
@@ -4,10 +4,22 @@ import (
 	"fmt"
 	"github.com/thatha/tweety-server-startup/reusable"
 	"log"
+	"bitbucket.org/kardianos/service"
 )
 
 func main() {
 	app_name := "tweety-server-startup"
+	desc := "Tweet on Startup"
+
+	var s, err = service.NewService(app_name, desc, desc)
+
+	err = s.Install()
+	if err != nil {
+		fmt.Printf("Failed to install: %s\n", err)
+		return
+	}
+	fmt.Printf("Service \"%s\" installed.\n", app_name)
+
 	config := reusable.GetConfig(app_name)
 	token, should_save_config, err := reusable.GetOauthCredentials(config)
 	if err != nil {
